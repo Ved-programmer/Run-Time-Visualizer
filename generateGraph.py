0d1b173bc26c23f1e1b749c3e240c032a9120edc
Ved Rathi <65692297+Ved-programmer@users.noreply.github.com>
2021-03-18 12:00:45 +0530

    Update generateGraph.py

--- generateGraph.py
+++ generateGraph.py
@@ -1,1 +1,49 @@
+import time
+import matplotlib.pyplot as plt
+import threading
 
+runTimes = False
+
+
+def getRunTime(func):
+    def wrapper(userInput):
+        start = time.time()
+        func(userInput)
+        return time.time() - start
+    return wrapper
+
+
+def getData(userFile):
+    global runTimes
+
+    exec(f"import {userFile}")
+    inputTimes = eval(f"{userFile}.inputTimes")
+    # print(inputTimes)
+    userFunc = getRunTime(eval(f"{userFile}.main"))
+
+    tempRunTimes = {}
+    for key, value in inputTimes.items():
+        tempRunTimes[key] = userFunc(value)
+    
+
+    runTimes = tempRunTimes
+
+
+
+
+def createGraph(runTimes):
+    plt.plot(list(runTimes.keys()), list(runTimes.values()))
+
+    plt.xlabel('Size Of Input') 
+    plt.ylabel('Run Time(in seconds)') 
+    
+    plt.title('Yo Tim')
+    
+    plt.show()
+    
+
+def generateGraphFromFile(userFile):
+    getData(userFile)
+    createGraph(runTimes)
+
+# generateGraphFromFile("sample")
